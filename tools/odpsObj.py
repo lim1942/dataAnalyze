57108fb2a29bd1e93b0bd8b12876f74f3e5afb04
lim <lim1942@163.com>
2019-07-11 10:35:44 +0800

    3

--- tools/odpsObj.py
+++ tools/odpsObj.py
@@ -1,22 +1,10 @@
-import sys
 import os
-# if sys.getsizeof(str_message) >8*1024*1000:
+import sys
+import time
+import requests
+
 from odps import ODPS
 
-# INSERT INTO TABLE zqh_auth_20190620(message,pt)
-# SELECT *
-# FROM fengdun_model_contrast
-# WHERE pt BETWEEN '2019_06_19_00_00' AND '2019_06_20_00_00'
-# AND GET_JSON_OBJECT(message,'$.appName')='攒qh' AND GET_JSON_OBJECT(message,'$.ver')
-
-
-# SELECT *
-# FROM
-# fengdun_model_contrast
-# WHERE
-# pt = MAX_PT('fengdun_model_contrast')
-# LIMIT
-# 10
 
 class myOdps:
 
@@ -34,13 +22,16 @@ class myOdps:
         return table_name
 
     # 创建一张表
-    def creat_table(self,table_name,columns,if_not_exists=True,lifecycle=28):
+    def creat_table(self,table_name,columns=None,if_not_exists=True):
         # table_name: 表名
         # columns :  ('num bigint, num2 double', 'pt string') 字段和分组的元组
         # if_not_exists:True   不存在才创建
         # lifecycle:28   生命周期
         # return 返回表对象
-        return self.odps.create_table(table_name,columns,if_not_exists=if_not_exists,lifecycle=lifecycle)
+        try:
+            return self.odps.create_table(table_name,columns,if_not_exists=if_not_exists)
+        except:
+            return self.odps.get_table(table_name)
 
     # 通过表名直接获取一张表
     def get_a_table(self,table_name):
@@ -64,15 +55,76 @@ class myOdps:
         return partitions
 
 
-    # 向一张表写入多条数据，
-    def write_to_table(self,table,records_list,partition=None,**kw):
-        # table: 表对象
-        # records_list:表数据对象的列表
-        #多个block能多线程写入
-        blocks = [block for block in range(len(records_list))]
-        with table.open_writer(partition=partition, blocks=blocks, **kw) as writer:
-            for block, records in zip(blocks, records_list):
-                writer.write(block,records)
+    # ============= 数据上传 ============
+    # 上传csv到odps并创建表，csv必须要有表头
+    def uploadCSV(self,csvFilename,tableName,sep=","):
+        """
+        :param csvFilename: 传入本地csv的路径，必须要有表头
+        :param tableName:  上传到odps时的表名
+        :param sep:   csv的分隔符
+        """
+        print("start upload ...\n")
+        f = open(csvFilename,encoding='utf-8')
+        lines = f.readlines()
+        _len = len(lines) - 2
+        f.close()
+        head = lines[0].strip('\n').split(sep)
+        pt_time = time.strftime("%Y-%m-%d")
+        head.append("pt")
+        head_list = [f"{x} string " for x in head]
+        head_line = ', '.join(head_list)
+        table = self.creat_table(tableName,head_line)
+        with table.open_writer(partition=None) as writer:
+            for index,line in enumerate(lines[1:]):
+                print(f"{index} /{_len} in {tableName}  ...")
+                item = line.strip('\n').split(sep)
+                item.append(pt_time)
+                writer.write(item)
+        print("\n\n upload finish ...")
+
+
+    # 上传的过程中并进行下载，下载完再上传完整的数据,数据行的坐标为1的字段为下载地址
+    def downloaAndUp(self,csvFilename,tableName,sep=","):
+        """
+        :param csvFilename: 传入本地csv的路径，必须要有表头
+        :param tableName:  上传到odps时的表名
+        :param sep:   csv的分隔符
+        """
+        def download(url, meth="get"):
+            if meth == "get":
+                while 1:
+                    try:
+                        resp = requests.get(url)
+                        return resp
+                    except:
+                        print("error in download")
+                        time.sleep(1)
+
+        print("start upload ...\n")
+        f = open(csvFilename,encoding='utf-8')
+        lines = f.readlines()
+        _len = len(lines) - 2
+        f.close()
+        head = lines[0].strip('\n').split(sep)
+        pt_time = time.strftime("%Y-%m-%d")
+        head.append("pt")
+        head_list = [f"{x} string " for x in head]
+        head_line = ', '.join(head_list)
+        table = self.creat_table(tableName,head_line)
+        with table.open_writer(partition=None) as writer:
+            for index, line in enumerate(lines[1:]):
+                print(f"{index} /{_len} in {tableName}  ...")
+                item = line.strip('\n').split(sep)
+                item.append(pt_time)
+                resp = download(item[1])
+                resp.encoding = "utf-8"
+                data = resp.text
+                if sys.getsizeof(data) <= 8 * 1024 * 1000:
+                    item[1] = data
+                    writer.write(item)
+                else:
+                    print(f"failed in {item[0]}")
+        print("\n\n upload finish ...")
 
 
     # ===========执行sql=========
@@ -81,22 +133,19 @@ class myOdps:
         # return: 查询结果的迭代对象
         with self.odps.execute_sql(sql).open_reader() as reader:
             return reader
-    # 其它sql
-    def insert_sql(self,sql):
-        # retrun: 插入的结果
-        return self.odps.execute_sql(sql)
+
 
 
 
 if __name__ == "__main__":
     obj = myOdps(access_id=os.getenv("ALIAK"),secret_access_key=os.getenv("ALIAKPWD"),project="shanghai")
-    # print(obj.get_a_table("zqh_repay_0614_0620"))
 
-    sql = """SELECT count(*)
-    FROM
-    zqh_repay_all
-    """
-
-    res = obj.select_sql(sql)
-    for i in res:
-        print(i)
+    # sql = """SELECT count(*)
+    # FROM
+    # zqh_repay_all
+    # """
+    #
+    # res = obj.select_sql(sql)
+    # for i in res:
+    #     print(i)
+    obj.uploadCSV("/Users/apple/Documents/project/analyze/strategy/new/strategy_judge_convert.csv","mycsvtest")
